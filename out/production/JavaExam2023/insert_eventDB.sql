c628776728560bbf4e297eec5c6cefb1a181ef4a
khvitmyhr <144025935+khvitmyhr@users.noreply.github.com>
2024-10-02 08:50:28 +0200

    Delete out directory

--- out/production/JavaExam2023/insert_eventDB.sql
+++
@@ -1,7 +0,0 @@
-USE eventDB;
-
-INSERT INTO attend (studentName, program, guest)
-VALUES('Kjell', 'Programmering', 'Marit');
-
-INSERT INTO attend (studentName, program, guest)
-VALUES('Bjørn', 'Databaser', 'Lise');
